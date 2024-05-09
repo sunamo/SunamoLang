4946620b571ad3cbecc6959f993d11d1bcf80d68
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-09 04:42:05 +0200

    .

--- _sunamo/SHGetLines.cs
+++ _sunamo/SHGetLines.cs
@@ -6,6 +6,6 @@ internal class SHGetLines
     //internal static Func<string, List<string>> GetLines;
     internal static List<string> GetLines(string s)
     {
-        return s.Split(new String[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries).ToList();
+        return sv.Split(new string[] { v.Contains("\r\n") ? "\r\n" : "\n" }, StringSplitOptions.RemoveEmptyEntries).ToList();
     }
 }
