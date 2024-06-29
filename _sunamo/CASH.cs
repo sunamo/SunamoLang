4321198c8314ec942ced58b5a5e2259643c7eb46
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-06-29 18:45:45 +0200

    All buildable

---
+++ _sunamo/CASH.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+internal class CASH
+{
+    public static List<string> Prepend(string v, List<string> toReplace)
+    {
+        for (int i = 0; i < toReplace.Count; i++)
+        {
+            if (!toReplace[i].StartsWith(v))
+            {
+                toReplace[i] = v + toReplace[i];
+            }
+        }
+        return toReplace;
+    }
+
+    public static string Replace(string s, string from, string to)
+    {
+        return s.Replace(from, to);
+    }
+
+    internal static void Replace(List<string> files_in, string what, string forWhat)
+    {
+        for (int i = 0; i < files_in.Count; i++)
+        {
+            files_in[i] = Replace(files_in[i], what, forWhat);
+        }
+        //CAChangeContent.ChangeContent2(null, files_in, Replace, what, forWhat);
+    }
+}
+
