40475117a5d58584a873fe2b7742d54b7db0f93e
sunamo.cz <radek.jancik@sunamo.cz>
2024-07-16 09:59:14 +0200

    before the second approach to remove *SunamoExceptions.cs

---
+++ SunamoXlf/TF.cs
@@ -0,0 +1,47 @@
+namespace SunamoLang.SunamoXlf;
+
+/// <summary>
+///
+/// </summary>
+internal class TF
+{
+    #region For easy copy
+    public static List<byte> bomUtf8 = new List<byte> { 239, 187, 191 };
+
+    public static
+#if ASYNC
+    async Task
+#else
+void
+#endif
+RemoveDoubleBomUtf8(string path)
+    {
+        var b =
+#if ASYNC
+    await
+#endif
+_sunamo.SunamoExceptions.InSunamoIsDerivedFrom.TF.ReadAllBytes(path);
+        var to = b.Count > 5 ? 6 : b.Count;
+
+        for (int i = 3; i < to; i++)
+        {
+            if (bomUtf8[i - 3] != b[i])
+            {
+                break;
+            }
+        }
+
+        b = b.Skip(3).ToList();
+        WriteAllBytes(path, b);
+    }
+
+
+    #endregion
+
+    #region Only in *Xlf.cs
+    public static void WriteAllBytes(string file, List<byte> b)
+    {
+        _sunamo.SunamoExceptions.InSunamoIsDerivedFrom.TF.WriteAllBytes(file, b);
+    }
+    #endregion
+}
